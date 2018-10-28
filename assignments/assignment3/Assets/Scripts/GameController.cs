cc1f2caae5e1c14a0a98c54507f9f6b0ffea9b23
Daute Rodríguez Rodríguez <alu0100973914@ull.edu.es>
2018-10-28 10:09:07 +0000

    Added assignment 3

---
+++ assignments/assignment3/Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GameController : MonoBehaviour
+{
+    public FirstPersonController player;
+
+    public GameObject[] speedDownModifiers;
+
+    /** Events */
+    public delegate void Event();
+    public static event Event EventsToHandle;
+
+    /** Delegate patron */
+    public delegate void PlayerDelegate(FirstPersonController player);
+    public static PlayerDelegate currentPlayerAction;
+
+    /** Singleton patron */
+    public static GameController gameController;
+
+    void Awake()
+    {
+        if (gameController == null)
+        {
+            gameController = this;
+            DontDestroyOnLoad(this);
+        } else if (this != gameController)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            EventsToHandle();
+        }
+        if (currentPlayerAction != null)
+        {
+            currentPlayerAction(player);
+            currentPlayerAction = null;
+        }
+    }
+}
---
+++ assignments/assignment3/Assets/Scripts/SpeedDownScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class SpeedDownScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameController.currentPlayerAction = SpeedDown;
+        }
+    }
+
+    void SpeedDown(FirstPersonController player)
+    {
+        player.m_JumpSpeed *= 0.75f;
+        player.m_WalkSpeed *= 0.75f;
+        player.m_RunSpeed *= 0.75f;
+
+        foreach (GameObject speedDown in GameController.gameController.speedDownModifiers)
+        {
+            speedDown.transform.position = new Vector3(
+                speedDown.transform.position.x, speedDown.transform.position.y + 0.5f, speedDown.transform.position.z);
+        }
+    }
+}
